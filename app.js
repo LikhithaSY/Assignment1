a07a62b969d312f1b3420eed81c5cd598ad69f00
LikhithaSY <151895113+LikhithaSY@users.noreply.github.com>
2024-12-14 11:08:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// This script will add smooth scrolling to the navigation links
+$(document).ready(function() {
+    $("a").on("click", function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+    });
+});
